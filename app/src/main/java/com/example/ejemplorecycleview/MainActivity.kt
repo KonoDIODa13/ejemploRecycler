4217ec1a463e2767fa0a1fd6eed4df3eb31a8cb0
jaime.gonbra <jaime.gonbra@educa.jcyl.es>
2025-01-30 09:21:45 +0100

    recycler view terminado

--- app/src/main/java/com/example/ejemplorecycleview/MainActivity.kt
+++ app/src/main/java/com/example/ejemplorecycleview/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ejemplorecycleview
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -52,18 +54,40 @@ class MainActivity : AppCompatActivity() {
     fun iniciarComponentes() {
         mibinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mibinding.root)
+
         addButton = mibinding.fabAdd
         addButton.setOnClickListener {
-            data.add(Coche("mikasa"))
-            adaptadorRecycler.notifyItemInserted(data.size)
+            annadirCoche()
         }
+
         iniciarRecycler()
+    }
+
+    fun annadirCoche() {
+        var textField = EditText(this)
+        var builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setView(textField)
+        textField.hint = "Escriba la marca del coche."
+        builder.setPositiveButton("Añadir") { interf, id ->
+            if (textField.text.isNotEmpty()) {
+                data.add(Coche(textField.text.toString()))
+                adaptadorRecycler.notifyItemInserted(data.size)
+            }
+            interf.dismiss()
+        }
+        builder.setNegativeButton("Cancelar") { interf, id ->
+            interf.dismiss()
+        }
+        var alert = builder.create()
+        alert.show()
     }
 
     fun iniciarRecycler() {
         vistaRecycler = mibinding.recycler
-        vistaRecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
+        vistaRecycler.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        // el adaptador del recycler requiere de la lista de datos y de una funcion lambda.
+        // la lambda consiste en cuando se pulse en uno de los elementos, se cambie el campo seleccionado.
         adaptadorRecycler = AdaptadorRecycler(data) { posicion ->
             data.get(posicion).seleccionado = !data.get(posicion).seleccionado
             adaptadorRecycler.notifyItemChanged(posicion)
